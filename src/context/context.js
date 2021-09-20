3897294d3ce499c36bad0e4cf11e0d727612d17e
Almog Wertzberger <Almogish@gmail.com>
2021-09-20 18:14:00 +0300

    setting promise.allsettled

--- src/context/context.js
+++ src/context/context.js
@@ -29,7 +29,21 @@ const GithubProvider = ({ children }) => {
 
     if (response) {
       setGithubUser(response.data);
-      //more logic here
+      const { login, followers_url } = response.data;
+
+      await Promise.allSettled([
+        axios(`${rootUrl}/users/${login}/repos?per_page=100`),
+        axios(`${followers_url}?per_page=100`),
+      ]).then((results) => {
+        const [repos, followers] = results;
+        const status = "fulfilled";
+        if (repos.status === status) {
+          setRepos(repos.value.data);
+        }
+        if (followers.status === status) {
+          setFollowers(followers.value.data);
+        }
+      });
     } else {
       toggleError(true, "there is no user with that username");
     }
