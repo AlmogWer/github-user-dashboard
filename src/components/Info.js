bdc135d7129ed0fe35dd22cf68922213d36888a3
Almog Wertzberger <Almogish@gmail.com>
2021-09-22 10:21:51 +0300

    bug fixes

--- src/components/Info.js
+++ src/components/Info.js
@@ -71,8 +71,15 @@ const Wrapper = styled.section`
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
   gap: 1rem 2rem;
+
   @media (min-width: 640px) {
     grid-template-columns: repeat(auto-fill, minmax(260px, 1fr));
+  }
+  @media (max-width: 1200px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+  @media (max-width: 550px) {
+    grid-template-columns: 1fr;
   }
 
   .item {
